45c904deb167e3cbca1de40c72928ae3ac528e1e
Yiching_chiang <yiching_chiang@dwango.co.jp>
2013-08-28 15:19:55 +0900

    レイアウト修正

--- views/layout.php
+++ views/layout.php
@@ -3,59 +3,52 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<script src="/js/jquery-2.0.3.min.js"></script>
-	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-	<script type="text/javascript" src="/js/bootstrap.js"></script>
-	<script type="text/javascript">
-		jQuery(document).ready(function(){
-			jQuery('a[href^=http]')
-			.not('[href*="'+location.hostname+'"]')
-			.attr({target:"_blank"})
-			.addClass("ex_link")
-		;})
-	</script>
-	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
-	<link href="//cdnjs.bootcss.com/ajax/libs/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<style type="text/css">
-      body {
-        padding-top: 50px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-    </style>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php if (isset($title)): echo $this->escape($title).'-'; endif;?>Mochi Blog</title>
+	<!-- Bootstrap -->
+	<link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<style>
+	body {
+		padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+	}
+	</style>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.min.css">
+	<script src="/js/jquery-2.0.3.min.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery('a[href^=http]')
+		.not('[href*="'+location.hostname+'"]')
+		.attr({target:"_blank"})
+		.addClass("ex_link")
+	;})
+	</script>
 </head>
+
 <body>
-	<div class="navbar navbar-fixed-top">
-		<a class="navbar-brand" href="<?php echo $base_url;?>/">Mochi Blog</a>
-		<ul class="nav navbar-nav">
-		<?php if($session->isAuthenticated()):?>
-			<li class="active"><a href="<?php echo $base_url;?>/">Home</a></li>
-			<li><a href="<?php echo $base_url;?>/account">アカウント</a></li>
-			<li><a href="<?php echo $base_url;?>/rss">RSSリーダー</a></li>
-			<li><a href="<?php echo $base_url;?>/account/signout">ログアウト</a></li>
-		<?php else:?>
-			<li><a href="<?php echo $base_url;?>/account/signin">ログイン</a></li>
-			<li><a href="<?php echo $base_url;?>/account/signuo">アカウント登録</a></li>
-		<?php endif;?>
-		</ul>
+	<div class="navbar navbar-fixed-top navbar-inverse">
+		<div class="navbar-inner">
+			<a class="brand " href="<?php echo $base_url;?>/">Mochi Blog</a>
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+				<?php if($session->isAuthenticated()):?>
+					<li class="active"><a href="<?php echo $base_url;?>/">Home</a></li>
+					<li><a href="<?php echo $base_url;?>/account">アカウント</a></li>
+					<li><a href="<?php echo $base_url;?>/rss">RSSリーダー</a></li>
+					<li><a href="<?php echo $base_url;?>/account/signout">ログアウト</a></li>
+				<?php else:?>
+					<li><a href="<?php echo $base_url;?>/account/signin">ログイン</a></li>
+					<li><a href="<?php echo $base_url;?>/account/signup">アカウント登録</a></li>
+				<?php endif;?>
+			</ul>
+			</div><!--/.nav-collapse -->
+		</div>
 	</div>
 
 	<div id="main">
 		<?php echo $_content;?>
 	</div>
+
 
 </body>
 </html>
--- views/rss/index.php
+++ views/rss/index.php
@@ -2,110 +2,102 @@
 <h2>Rss Reader</h2>
 <script src="/js/test.js"></script>
 
-<form action= "<?php echo $base_url;?>/rss/add" method = "post">
-	<input type="hidden" name="_token" value="<?php echo $this->escape($_token);?>" id="token">
-	<?php if(isset($errors) && count($errors) > 0 ):?>
-	<?php echo $this->render('errors' , array('errors' => $errors));?>
-	<?php endif;?>
-	<input class="span2"  type = "text" name="url" size="100" id="url">
-	<p><input type="submit" id="addbtn" value="追加"></p>
-	</div>
-</form>
-<hr>
-
 <div class="container-fluid">
-	<div class="row-fluid">
-		<div class="col-lg-9 col-lg-push-3" id="content">
-			<?php if(count($entries) === 0):?>
-			<h3>Rssを追加してください。</h3>
-		<?php else:?>
-			<?php foreach($entries as $entry):?>
-			<?php echo $this->render('rss/rss',array('entry' => $entry));?>
-			<?php endforeach;?>
-		<?php endif;?>
-		</div>
-		<div class="col-lg-3 col-lg-pull-9">
-			<div id="addcategory">
-				<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
-					<input class="span4"  type = "text" name="category" size="100" >
-					<p><input type="submit" id="addbtn" value="追加"></p>
-				</form>
+<div class="row-fluid">
+	<div class="span12">
+
+		<div class="input-append">
+			<form action= "<?php echo $base_url;?>/rss/add" method = "post">
+				<input type="hidden" name="_token" value="<?php echo $this->escape($_token);?>" id="token">
+				<?php if(isset($errors) && count($errors) > 0 ):?>
+				<?php echo $this->render('errors' , array('errors' => $errors));?>
+				<?php endif;?>
+				<input class="span2"  type = "text" name="url" size="100" id="appendedInputButton">
+				<button class="btn" type="submit" id="addbtn">Go!</button>
+			</form>
+			<div id="modal">
 			</div>
+		</div>
 		<hr>
-			<div class="well sidebar-nav">
-				<ul class="nav nav-list"　>
-					<li class="nav-header"><a href="<?php echo $base_url;?>/rss">RSSホーム</a></li>
+
+		<div class="row-fluid">
+			<div class="span9" id="content">
+				<?php if(count($entries) === 0):?>
+				<h3>Rssを追加してください。</h3>
+				<?php else:?>
+					<?php foreach($entries as $entry):?>
+					<?php echo $this->render('rss/rss',array('entry' => $entry));?>
+					<?php endforeach;?>
+				<?php endif;?>
+			</div>
+
+			<div class="span3">
+				<div id="addcategory">
+					<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
+						<input class="span4"  type = "text" name="category" size="100" >
+						<p><input type="submit" id="addbtn" value="追加"></p>
+					</form>
+				</div>
+				<hr>
+				<div class="well sidebar-nav">
+					<ul class="nav nav-list"　>
+						<li class="nav-header"><a href="<?php echo $base_url;?>/rss">RSSホーム</a></li>
 
 					<div>
-					<?php foreach($categories as $category => $sites):?>
-						<?php $options .="<option value='".$category."'>". $category."</option>";?>
-					<?php endforeach;?>
+						<?php foreach($categories as $category => $sites):?>
+							<?php $options .="<option value='".$category."'>". $category."</option>";?>
+						<?php endforeach;?>
 					</div>
-				<?php foreach($categories as $category => $sites):?>
-				<?php if($category !== 'uncategorized'):?>
-					<li id="category" class="active" data-id="<?php echo $this->escape($category);?>"><strong><?php echo $this->escape($category);?></strong></li>
+<!-- RSSリスト -->
+					<div>
+						<?php foreach($categories as $category => $sites):?>
+						<?php if($category !== 'uncategorized'):?>
+						<li id="category" class="active" data-id="<?php echo $this->escape($category);?>"><strong><?php echo $this->escape($category);?></strong></li>
+							<ul>
+								<?php foreach($sites as $key =>$site):?>
+								<?php if(($site['site_id']) !== 'null'):?>
+								<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
+									<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
+									<span class="delete">X</span>
+									<div name="categorize">
+										<form method="POST" action ="<?php echo $base_url;?>/rss/categorize">
+											<input type="hidden" name="site_id" value="<?php echo $this->escape($site['site_id']);?>">
+											<select name="test"><?php echo $options?></select>
+											<input type="submit" value="送信">
+										</form>
+									</div>
+								</li>
+								<?php endif;?>
+								<?php endforeach;?>
+							</ul>
+						<?php else:?>
+						<br>
 						<ul>
 							<?php foreach($sites as $key =>$site):?>
-							<?php if(($site['site_id']) !== 'null'):?>
-							<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
-								<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
-								<span class="delete">X</span>
-								<div name="categorize">
-								<form method="POST" action ="<?php echo $base_url;?>/rss/categorize">
-									<input type="hidden" name="site_id" value="<?php echo $this->escape($site['site_id']);?>">
-									<select name="test"><?php echo $options?></select>
-									<input type="submit" value="送信">
-								</form>
-								</div>
-							</li>
+								<?php if($site['site_id'] !=='null'):?>
+								<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
+									<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
+									<span class="delete">X</span>
+									<div name="categorize">
+										<form method="POST" action ="<?php echo $base_url;?>/rss/categorize">
+											<input type="hidden" name="site_id" value="<?php echo $this->escape($site['site_id']);?>">
+											<select name="test"><?php echo $options?></select>
+											<input type="submit" value="送信">
+										</form>
+									</div>
+								</li>
+								<?php endif;?>
+							<?php endforeach;?>
+						</ul>
 						<?php endif;?>
 						<?php endforeach;?>
-						</ul>
-					<?php else:?>
-					<ul>
-						<br>
-							<?php foreach($sites as $key =>$site):?>
-							<?php if($site['site_id'] !=='null'):?>
-							<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
-								<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
-								<span class="delete">X</span>
-								<div name="categorize">
-								<form method="POST" action ="<?php echo $base_url;?>/rss/categorize">
-									<input type="hidden" name="site_id" value="<?php echo $this->escape($site['site_id']);?>">
-									<select name="test"><?php echo $options?></select>
-									<input type="submit" value="送信">
-								</form>
-								</div>
-							</li>
-						<?php endif;?>
-						<?php endforeach;?>
-						</ul>
-					<?php endif;?>
-				<?php endforeach;?>
-				</ul>
-			</div>
+					</div>
+					</ul>
+				</div>
 			<hr>
 			</div>
-<!--
-			<div class="well sidebar-nav">
-			<ul id="lists" class="nav nav-list"　>
-				<li class="nav-header"><a href="<?php echo $base_url?>/rss">RSSホーム</a></li>
-				<?php foreach ($siteTitles as $siteTitle):?>
-				<li class="active" id="siteTitleId_<?php echo $this->escape($siteTitle['site_id']);?>" data-id="<?php echo $this->escape($siteTitle['site_id']);?>"><a id="blog"><?php echo $this->escape($siteTitle['site_title']);?></a>
-					<span class="delete">X</span>
-				</li>
-				<?php endforeach;?>
-
-			</ul>
-			</div><!--/.well -->
 		</div>
+
 	</div>
 </div>
-
-
-
-
-
-
-
-
+</div>
