6563bf4ab9ca1660ea81ab5fabe3860796e308b8
Yiching_chiang <yiching_chiang@dwango.co.jp>
2013-08-28 17:09:29 +0900

    モーダル使えるようになった！

--- views/rss/index.php
+++ views/rss/index.php
@@ -15,7 +15,10 @@
 				<input class="span2"  type = "text" name="url" size="100" id="appendedInputButton">
 				<button class="btn" type="submit" id="addbtn">Go!</button>
 			</form>
+
 			<div id="modal">
+				<button type="button" data-toggle="modal" data-target="#myModal">執行對話視窗</button>
+
 			</div>
 		</div>
 		<hr>
@@ -32,31 +35,43 @@
 			</div>
 
 			<div class="span3">
-				<div id="addcategory">
+				<div>
+					<?php foreach($categories as $category => $sites):?>
+						<?php $options .="<option value='".$category."'>". $category."</option>";?>
+					<?php endforeach;?>
+				</div>
+<!-- 				<div id="addcategory">
 					<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
 						<input class="span4"  type = "text" name="category" size="100" >
 						<p><input type="submit" id="addbtn" value="追加"></p>
 					</form>
 				</div>
-				<hr>
+				<hr>-->
 				<div class="well sidebar-nav">
 					<ul class="nav nav-list"　>
-						<li class="nav-header"><a href="<?php echo $base_url;?>/rss">RSSホーム</a></li>
-
-					<div>
-						<?php foreach($categories as $category => $sites):?>
-							<?php $options .="<option value='".$category."'>". $category."</option>";?>
-						<?php endforeach;?>
-					</div>
-<!-- RSSリスト -->
-					<div>
+						<li class="nav-header">
+							<h4>RSSList
+								<div class="btn-group pull-right">
+								<a class="btn btn-small dropdown-toggle" data-toggle="dropdown" href="#">
+								設定
+								<span class="caret"></span>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="#myModal"  data-toggle="modal">カテゴリ追加</a></li>
+									<li><a href="#myModal2" data-toggle="modal">RSS追加</a></li>
+								</ul>
+								</div>
+							</h4>
+						</li>
+						<li class="active"><a href="<?php echo $base_url;?>/rss"><i class="icon-home icon-white"></i>RSSホーム</a></li>
+						<!-- RSSリスト -->
 						<?php foreach($categories as $category => $sites):?>
 						<?php if($category !== 'uncategorized'):?>
-						<li id="category" class="active" data-id="<?php echo $this->escape($category);?>"><strong><?php echo $this->escape($category);?></strong></li>
-							<ul>
+						<li id="category" data-id="<?php echo $this->escape($category);?>"><a><strong><i class="icon-list"></i><?php echo $this->escape($category);?></strong></a></li>
+							<ul class="unstyled">
 								<?php foreach($sites as $key =>$site):?>
 								<?php if(($site['site_id']) !== 'null'):?>
-								<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
+								<li class= "lists" id ="siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
 									<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
 									<span class="delete">X</span>
 									<div name="categorize">
@@ -72,12 +87,15 @@
 							</ul>
 						<?php else:?>
 						<br>
-						<ul>
+							<ul class="unstyled">
 							<?php foreach($sites as $key =>$site):?>
 								<?php if($site['site_id'] !=='null'):?>
-								<li class= "active lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
+								<li class= "lists" id = "siteId_<?php echo $this->escape($site['site_id']);?>" data-id="<?php echo $this->escape($site['site_id']);?>">
 									<a id = "blog"><?php echo $this->escape(mb_strimwidth($site['site_title'], 0, 35,"..."));?></a>
 									<span class="delete">X</span>
+									<i class="icon-wrench"></i>
+
+
 									<div name="categorize">
 										<form method="POST" action ="<?php echo $base_url;?>/rss/categorize">
 											<input type="hidden" name="site_id" value="<?php echo $this->escape($site['site_id']);?>">
@@ -91,7 +109,7 @@
 						</ul>
 						<?php endif;?>
 						<?php endforeach;?>
-					</div>
+
 					</ul>
 				</div>
 			<hr>
@@ -101,3 +119,65 @@
 	</div>
 </div>
 </div>
+
+
+				<!-- Modal -->
+				<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3 id="myModalLabel">カテゴリ追加</h3>
+					</div>
+					<div class="modal-body">
+						<div id="addcategory">
+							<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
+								<input class="span4"  type = "text" name="category" size="100" >
+								<p><input type="submit" id="addbtn" value="追加"></p>
+						</form>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button class="btn" data-dismiss="modal" aria-hidden="true">close</button>
+						<button class="btn btn-primary">save</button>
+					</div>
+				</div><!-- Modal -->
+
+				<!-- Modal 2-->
+				<div id="myModal2" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3 id="myModalLabel">カテゴリ追加</h3>
+					</div>
+					<div class="modal-body">
+						<div id="addcategory">
+							<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
+								<input class="span4"  type = "text" name="category" size="100" >
+								<p><input type="submit" id="addbtn" value="追加"></p>
+						</form>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button class="btn" data-dismiss="modal" aria-hidden="true">close</button>
+						<button class="btn btn-primary">save</button>
+					</div>
+				</div><!-- Modal -->
+
+
+				<!-- Modal 3-->
+				<div id="myModal3" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h3 id="myModalLabel">カテゴリ追加</h3>
+					</div>
+					<div class="modal-body">
+						<div id="addcategory">
+							<form action="<?php echo $base_url;?>/rss/addCategory" method="post">
+								<input class="span4"  type = "text" name="category" size="100" >
+								<p><input type="submit" id="addbtn" value="追加"></p>
+						</form>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button class="btn" data-dismiss="modal" aria-hidden="true">close</button>
+						<button class="btn btn-primary">save</button>
+					</div>
+				</div><!-- Modal -->
